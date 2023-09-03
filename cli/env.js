577bcd215d57e82f3d007e4bc0dce427adc9c406
Robert Adams <adamsiii.robert@gmail.com>
2023-09-02 18:22:05 -0700

    add vars

--- cli/env.js
+++ cli/env.js
@@ -1,13 +1,23 @@
 import { open, appendFile } from "fs/promises";
 import { logger } from "./logger.js";
-import { ENV, PATH } from "./utils.js";
+
+export const FILE = ".env";
+
+const ENV = {
+  DB_NAME: "ecommerce_db",
+  DB_USER: "root",
+  DB_PASS: "password",
+  // fallback var names
+  DB_USERNAME: "root",
+  DB_PASSWORD: "password",
+};
 
 export const env = {
   async default() {
     try {
-      const fileHandle = await open(PATH, "w");
+      const fileHandle = await open(FILE, "w");
       for (const [key, val] of Object.entries(ENV)) {
-        await appendFile(PATH, `${key}="${val}"\n`);
+        await appendFile(FILE, `${key}="${val}"\n`);
       }
       await fileHandle.close();
       logger.ok(".env generated");
