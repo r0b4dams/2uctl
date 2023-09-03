b926688ebce6e7bc1d1eb2e9f2862a39c45200c6
Robert Adams <adamsiii.robert@gmail.com>
2023-09-02 17:18:16 -0700

    add fallback prop for db username

--- cli/utils.js
+++ cli/utils.js
@@ -9,9 +9,13 @@ export const VERSION = pkg.version;
 export const PATH = ".env";
 
 export const ENV = {
+  // m13
   DB_USERNAME: "root",
+  DB_USER: "root",
   DB_PASSWORD: "password",
   DB_PASS: "password",
   DB_NAME: "ecommerce_db",
+
+  // m18
   MONGODB_URI: "mongodb://127.0.0.1:27017/social_media_api",
 };
