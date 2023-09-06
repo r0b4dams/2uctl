6cd3cb746581e997ac83bf839e7ff4dcf08baa8a
Robert Adams <adamsiii.robert@gmail.com>
2023-09-06 02:03:45 -0700

    extend try block

--- cli/index.js
+++ cli/index.js
@@ -2,21 +2,21 @@ import { Command } from "commander";
 import { generateEnv } from "./env.js";
 import { collect, logger, version } from "../utils/index.js";
 
-const program = new Command();
-
-program
-  .name("askbcs")
-  .description("A CLI to help automate stuff")
-  .version(version);
-
-program
-  .command("env")
-  .description("Generate a .env file")
-  .argument("[keyval...]", "optionally set env vars", collect, [])
-  .action(generateEnv)
-  .hook("postAction", () => logger.ok(".env generated"));
-
 try {
+  const program = new Command();
+
+  program
+    .name("askbcs")
+    .description("A CLI to help automate stuff")
+    .version(version);
+
+  program
+    .command("env")
+    .description("Generate a .env file")
+    .argument("[keyval...]", "optional list of env vars", collect, [])
+    .action(generateEnv)
+    .hook("postAction", () => logger.ok(".env generated"));
+
   program.parse();
 } catch (error) {
   logger.err(error.message);
