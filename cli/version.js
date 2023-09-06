ee051338e029f13e2db21243510b82ebffc8ed97
Robert Adams <adamsiii.robert@gmail.com>
2023-09-06 00:10:18 -0700

    mv to utils

--- cli/version.js
+++
@@ -1,7 +0,0 @@
-import { readFile } from "fs/promises";
-
-const packageJSON = JSON.parse(
-  await readFile(new URL("../package.json", import.meta.url))
-);
-
-export const semver = packageJSON.version;
