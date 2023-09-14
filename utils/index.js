21bdd1e462411bce91c9b47a586568b759610ab5
Robert Adams <adamsiii.robert@gmail.com>
2023-09-13 19:33:10 -0700

    update exports

--- utils/index.js
+++ utils/index.js
@@ -1,3 +1,5 @@
 export { collect } from "./collect.js";
+export { filesearch } from "./filesearch.js";
 export { logger } from "./logger.js";
+export { validate } from "./validate.js";
 export { version } from "./version.js";
