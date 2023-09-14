3c1367cdbc5abac488ea9c7eef8623efb0054ce6
Robert Adams <adamsiii.robert@gmail.com>
2023-09-13 19:32:30 -0700

    get user input, extend question method

---
+++ cli/readline.js
@@ -0,0 +1,14 @@
+// caution: this module is currently experimental
+// https://nodejs.org/api/readline.html#promises-api
+import readline from "readline/promises";
+import chalk from "chalk";
+
+export const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.ask = async (question) =>
+  rl.question(
+    `${chalk.blue("[askbcs]")} ${chalk.cyan("[prompt]")} ${question}`
+  );
