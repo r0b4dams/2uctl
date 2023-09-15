0299c506106b27812f15d5194d943f8a498716d2
Robert Adams <adamsiii.robert@gmail.com>
2023-09-15 00:32:23 -0700

    add basic checks for existing files/dirs

--- utils/validate.js
+++ utils/validate.js
@@ -14,21 +14,3 @@ async function hasEnv() {
 async function hasPackageJson() {
   return hasPath(process.cwd(), "package.json");
 }
-
-async function test() {
-  if (await isRepo()) {
-    console.log(".git found");
-  } else {
-  }
-
-  if (await hasPackageJson()) {
-    console.log("package.json found");
-  } else {
-  }
-
-  if (await hasEnv()) {
-    console.log(".env found");
-  } else {
-    console.log(".env not found");
-  }
-}
