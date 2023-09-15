eb2379b73771264bf88d022ca075c2648f34762a
Robert Adams <adamsiii.robert@gmail.com>
2023-09-14 18:26:34 -0700

    rename file

--- utils/io.js
+++ utils/io.js
@@ -3,12 +3,10 @@
 import readline from "readline/promises";
 import chalk from "chalk";
 
-export const rl = readline.createInterface({
+export const io = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-rl.ask = async (question) =>
-  rl.question(
-    `${chalk.blue("[askbcs]")} ${chalk.cyan("[ask]")} ${question}`
-  );
+io.ask = async (question) =>
+  io.question(`${chalk.blue("[askbcs]")} ${chalk.cyan("[ask]")} ${question}`);
