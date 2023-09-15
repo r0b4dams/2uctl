cd3c52ab329aefdfe4b8b3202380309e5b3e926b
Robert Adams <adamsiii.robert@gmail.com>
2023-09-15 00:26:39 -0700

    complete happy path for setup command

--- cli/commands/setup.js
+++ cli/commands/setup.js
@@ -1,106 +1,101 @@
-import { resolve } from "path";
-import { exec } from "child_process";
+import {
+  execute,
+  findPath,
+  getCredentials,
+  getDbName,
+  logger,
+  sourceFile,
+} from "../../utils/index.js";
 
-import { rl } from "../readline.js";
-import { logger } from "../../utils/index.js";
-
-export function setup(module, _options) {
-  if (!["m12", "m13", "m14"].includes(module)) {
-    throw new Error("invalid module given");
-  }
-
-  switch (module) {
-    case "m12":
-      m12();
-      break;
-    case "m13":
-      m13();
-      break;
-    case "m14":
-      m14();
-      break;
-    default:
-      throw new Error("module not recognized");
-  }
-}
-
-//
-const defaults = {
-  un: "root",
-  pw: "password",
-  src: "schema.sql",
-};
-
-async function mysql() {
-  let un = await rl.ask(`Enter mysql username (${defaults.un}): `);
-  if (!un) {
-    un = defaults.un;
-  }
-  let pw = await rl.ask(`Enter mysql password (${defaults.pw}): `);
-  if (!pw) {
-    pw = defaults.pw;
-  }
-  rl.close();
-
-  logger.info("Connecting to MySQL...");
-
-  const mysql = exec(
-    `mysql -u${un} -p${pw} -e "source ${resolve("db", defaults.src)}"`,
-    (error, stdout) => {
-      if (!error) {
-        logger.ok("Database sourced successfully!");
-      } else if (stdout) {
-        logger.info(stdout);
-      }
-    }
-  );
-
-  mysql.stderr.on("data", (output) => {
-    if (output.includes("ERROR")) {
-      logger.warn("Could not source schema.sql");
-      logger.err(output.trim());
-    }
-  });
-
-  mysql.on("close", (code) => {
-    logger.info(`MySQL disconnected with exit code ${code}`);
-  });
-}
-
-function m12() {
-  console.log("setup module 12 challenge env");
-  mysql();
-}
-
-function m13() {
-  console.log("setup module 13 challenge env");
-  // search for schema.sql or db/schema.sql
-  // if not found throw err
-  // prompt for mysql un and pw to create database (and add to ENV)
-}
-
-function m14() {
-  console.log("setup module 14 challenge env");
-  // search for schema.sql or db/schema.sql
-  // if not found throw err
-  // prompt for mysql un and pw to create database (and add to ENV)
-}
+import { env } from "./env.js";
 
 /**
- * m12
- * prompt user for input:
- * mysql username? (root)
- * mysql password? (password)
- *
- * want to find schema.sql file
- * if found,
- * source it via exec method
- *
- * want to find seed.sql file
- * if found,
- * source it via exex method
- *
- * I'd love to be able to update the connection as well,
- * but there are too many ways students can set up this logic
- * many students don't modularize this assignment, for example
+ * Setup the development environment for the SQL modules (12, 13, & 14)
+ * @param {string} module "m12" | "m13" | "m14"
  */
+export async function setup(module) {
+  try {
+    if (!["m12", "m13", "m14"].includes(module)) {
+      throw new Error("invalid module given");
+    }
+
+    switch (module) {
+      case "m12":
+        logger.init("Setup Module 12 - Employee Tracker");
+        break;
+      case "m13":
+        logger.init("Setup Module 13 - E-Commerce Back End");
+        break;
+      case "m14":
+        logger.init("Setup Module 14 - Tech Blog");
+        break;
+    }
+
+    const schemaPath = await findPath(process.cwd(), "schema.sql");
+
+    if (schemaPath) {
+      logger.info(`Schema file found @ ${schemaPath}`);
+    } else {
+      logger.warn("Schema file not found");
+      throw new Error("Schema file not found");
+    }
+
+    const db = await getDbName(schemaPath);
+    logger.info("Database name:", db);
+
+    const [un, pw] = await getCredentials();
+    await sourceFile(un, pw, schemaPath);
+
+    logger.info("Generating .env file...");
+    await env([`DB_NAME=${db}`, `DB_USER=${un}`, `DB_PASS=${pw}`]);
+    logger.info(".env generated!");
+
+    switch (module) {
+      case "m12":
+        m12(un, pw);
+        break;
+      case "m13":
+        m13();
+        break;
+      case "m14":
+        m14();
+        break;
+    }
+  } catch (error) {
+    logger.error(error.message);
+  }
+}
+
+async function m12(un, pw) {
+  const seedPath =
+    (await findPath(process.cwd(), "seed.sql")) ||
+    (await findPath(process.cwd(), "seeds.sql"));
+
+  logger.info("Seeding database...");
+  if (seedPath) {
+    logger.info(`Seed file found @ ${seedPath}`);
+    await sourceFile(un, pw, seedPath);
+  } else {
+    logger.warn("Seed file not found");
+  }
+
+  logger.info("Installing dependencies...");
+  await execute("npm install");
+  logger.info("Dependencies installed!");
+}
+
+async function m13() {
+  logger.info("Installing dependencies...");
+  await execute("npm install");
+  logger.info("Dependencies installed!");
+
+  logger.info("Seeding database...");
+  await execute("npm run seed");
+  logger.info("Database seeded!");
+}
+
+async function m14() {
+  logger.info("Installing dependencies...");
+  await execute("npm install");
+  logger.info("Dependencies installed!");
+}
