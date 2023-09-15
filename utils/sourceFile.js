36a94124f7244971cf1bd7e25116731f3d6f7b05
Robert Adams <adamsiii.robert@gmail.com>
2023-09-15 00:31:56 -0700

    add promise wrapper around exec call to mysql cli

---
+++ utils/sourceFile.js
@@ -0,0 +1,29 @@
+import { exec } from "child_process";
+import { logger } from "./logger.js";
+
+/**
+ * invokes the mysql shell with the given credentials
+ * and executes the SQL SOURCE command on the given file
+ * @param {string} un mysql user
+ * @param {string} pw mysql password
+ * @param {string} src path to the sql file
+ * @returns {Promise<void>}
+ */
+export async function sourceFile(un, pw, src) {
+  return new Promise((resolve, reject) => {
+    const mysql = exec(`mysql -u${un} -p${pw} -e "SOURCE ${src}"`, (error) => {
+      if (error) {
+        reject(error);
+      }
+      logger.info(`Sourced ${src}`);
+      resolve();
+    });
+
+    mysql.stderr.on("data", (output) => {
+      if (output.includes("ERROR")) {
+        logger.warn(`Could not source file: ${src}`);
+        logger.error(output.trim());
+      }
+    });
+  });
+}
