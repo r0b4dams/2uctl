fc1169ffb4829abb37244c32c9aaf4a370d279fb
Robert Adams <adamsiii.robert@gmail.com>
2023-09-17 11:22:48 -0700

    update publish

---
+++ scripts/delete_tags.sh
@@ -0,0 +1,9 @@
+# Delete All local tags. (Optional Recommended)
+git tag -d $(git tag -l)
+# Fetch remote All tags. (Optional Recommended)
+git fetch
+# Delete All remote tags.
+# Note: pushing once should be faster than multiple times
+git push origin --delete $(git tag -l) 
+# Delete All local tags.
+git tag -d $(git tag -l)
