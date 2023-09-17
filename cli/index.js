aa4bd3da66a2c980b0df16b07c64422ebd6b806d
Robert Adams <adamsiii.robert@gmail.com>
2023-09-17 02:28:41 -0700

    refactor metadata

--- cli/index.js
+++ cli/index.js
@@ -1,30 +1,41 @@
 import { Command, Option } from "commander";
-import { env } from "./commands/index.js";
-import { collect, logger, version } from "../utils/index.js";
+
+import { env } from "./commands/env/index.js";
+import { collect } from "./utils/collect.js";
+import { logger } from "./utils/logger.js";
+import { metadata } from "./utils/metadata.js";
 
 try {
   const program = new Command();
 
   program
-    .name("askbcs")
-    .description("A CLI to help automate stuff")
-    .version(version);
+    .name(metadata.name)
+    .description(metadata.description)
+    .version(metadata.version);
 
   program
     .command("env")
+    .description("generate a .env file in the current directory.")
     .action(env)
-    .description("Generate a .env file")
-    .argument("[keyval...]", "optional list of env vars", collect, [])
-    .option("-u, --user <user>", "specify username")
-    .option("-p, --password [password]", "specify password")
+    .option("-s, --sql [sql]", "create an sql database")
+    .option("-u, --user <user>", "specify mysql username")
+    .option("-p, --password [password]", "specify mysql password")
+    .option("-d, --debug [debug]", "log debugging data")
     .addOption(
-      new Option(
-        "-m, --module <module>",
-        "setup dev env for modules 12, 13, 14"
-      ).choices(["12", "13", "14"])
+      new Option("-m, --module <module>", "specify module env setup").choices([
+        "12",
+        "13",
+        "14",
+      ])
+    )
+    .argument(
+      "[keyval...]",
+      "optional list of env vars with syntax KEY=VALUE",
+      collect,
+      []
     );
 
   program.parse();
 } catch (error) {
-  logger.error(error.message);
+  logger.error(error);
 }
