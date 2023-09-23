95dab040f5b9231d7585bede6a08c7cb15de6d67
Robert Adams <adamsiii.robert@gmail.com>
2023-09-23 10:02:23 -0700

    add comment

--- cli/utils/execute.js
+++ cli/utils/execute.js
@@ -1,6 +1,12 @@
 import { exec } from 'child_process';
 
-// TODO: sanitize inputs
+/**
+ * TODO: sanitize inputs
+ * executes the given command in a new shell with the given arguments
+ * @param {string} exe command to run
+ * @param  {string[]} args arguments / options for the given command
+ * @returns {Promise<void>}
+ */
 export async function execute(exe, ...args) {
   return new Promise((resolve, reject) => {
     const cmd = [exe, ...args].join(' ');
