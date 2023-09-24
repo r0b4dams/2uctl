5fd16da9230bbd47a420cb30f0a9838f7b1d5753
Robert Adams <adamsiii.robert@gmail.com>
2023-09-24 09:21:11 -0700

    hotfix/tag-push

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,7 +5,7 @@ TAG=v$VERSION
 git tag -a $TAG -m "release version $VERSION"
 
 if [ $? -eq 0 ]; then
-  git push origin $TAG
+  git push origin $TAG --follow-tags
 else
   echo error: failed to tag build
 fi
