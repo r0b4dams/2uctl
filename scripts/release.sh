c3e67486d4a1ee75530fbe82bdc6d60e26a9e9d0
Robert Adams <adamsiii.robert@gmail.com>
2023-09-24 11:55:50 -0700

    restore release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -6,10 +6,10 @@ git pull
 VERSION=$(node -p "require('./package.json').version")
 TAG=v$VERSION
 
-git tag -a $TAG -m "release version $VERSION"
+git tag -a $TAG -m "release $VERSION"
 
 if [ $? -eq 0 ]; then
-  git push --follow-tags
+  git push origin $TAG
 else
   echo error: failed to tag build
 fi
